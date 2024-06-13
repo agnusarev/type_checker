3b4f807769879113eaa4471ad6b993507972d30f
Alexander Gnusarev <alexander.gnussarev@gmail.com>
2024-06-13 21:43:19 +0600

    добавлены решения

---
+++ src/type_checker/extreme/variance/variance.py
@@ -0,0 +1,13 @@
+"""
+TODO: Annotate function `f` and `g`, to make tests pass.
+"""
+
+from typing import Sequence, Union
+
+
+def f(a: list[Union[int, str]]) -> None:
+    pass
+
+
+def g(a: Sequence[Union[int, str]]) -> None:
+    pass
